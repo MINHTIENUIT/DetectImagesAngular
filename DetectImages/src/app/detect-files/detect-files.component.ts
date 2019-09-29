e20ed16576806b6415337a15bb81de621c0c9d5f
MinhTienUIT <vanminhtien96@gmail.com>
2019-09-29 08:20:35 +0700

    Add bootstrap

--- DetectImages/src/app/detect-files/detect-files.component.ts
+++ DetectImages/src/app/detect-files/detect-files.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-detect-files',
   templateUrl: './detect-files.component.html',
-  styleUrls: ['./detect-files.component.css']
+  styleUrls: ['./detect-files.component.css',
+    '../../node_modules/bootstrap/dist/css/bootstrap.min.css'    
+  ]
 })
 export class DetectFilesComponent implements OnInit {
 
